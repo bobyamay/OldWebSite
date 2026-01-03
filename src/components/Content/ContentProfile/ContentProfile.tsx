b886b0d9aa33af389839555dae11292d8bc3ae33
yujiyama <bobwo0927@gmail.com>
2026-01-03 10:06:52 +0000

    プロフィール修正

--- src/components/Content/ContentProfile/ContentProfile.tsx
+++ src/components/Content/ContentProfile/ContentProfile.tsx
@@ -2,6 +2,12 @@ import PageTitle from '../../Common/PageTitle';
 import './ContentProfile.css';
 
 export default function ContentProfile() {
+
+  const birthDate: string = '2000/01/02';
+  const birthYear: number = parseInt(birthDate.split('/')[0]);
+  const currentYear: number = new Date().getFullYear();
+  const age: number = currentYear - birthYear;
+
   return (
     <div className="main-content-profile">
       <PageTitle title="プロフィール" />
@@ -10,10 +16,12 @@ export default function ContentProfile() {
           <img className="profile-image" src="/src/image/profile.jpeg" alt="プロフィール画像" />
         </div>
         <div className="content-right">
-          <h3 className="profile-name">名前 : </h3>
+          <h3 className="profile-title">名前  </h3>
           <p >山田太郎</p>
-          <h3 className="profile-name">年齢 :</h3>
-          <p >25</p>
+          <h3 className="profile-title">誕生日 </h3>
+          <p >{birthDate}</p>
+          <h3 className="profile-title">年齢 </h3>
+          <p >{age}</p>
         </div>
       </div>
     </div>
